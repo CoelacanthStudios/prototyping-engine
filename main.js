9c687df48874ac38bfe3e3f899b32773c656571b
Mazz1224 <mazz1224@gmail.com>
2018-09-05 20:08:04 -0400

    Starting over

--- main.js
+++ main.js
@@ -1,31 +1,7 @@
-let player;
-
-class Player {
-	constructor(spawn) {
-		this.pos = createVector(spawn.x, spawn.y);
-	}
-
-	update() {}
-
-	input() {
-		if(keyCode === 87) {
-			this.pos.y -= 10;
-		}
-	}
-
-	render() {
-		noStroke();
-		fill(0);
-		ellipse(this.pos.x, this.pos.y, 100);
-	}
-}
-
 function setup() {
 	width = windowWidth;
 	height = windowHeight;
 	createCanvas(width, height);
-
-	player = new Player({x: width / 2, y: height / 2});
 }
 
 function draw() {
@@ -33,12 +9,6 @@ function draw() {
 
 	player.update();
 	player.render();
-}
-
-function keyIsDown(code) {
-	if(code === 87 || code === 83 || code === 65 || code === 68) {
-		player.input();
-	}
 }
 
 function windowResized() {
